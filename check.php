f6faa530ff4109546fc771710ad4446a00d387ff
Benjamin Pearson <ben.pearson@swoopanalytics.com>
2023-10-10 18:10:15 +1100

    adding some more debug.

--- check.php
+++ check.php
@@ -2,3 +2,15 @@
 
 var_dump(getenv());
 
+$baseRef = (getenv('GITHUB_BASE_REF') ?: 'master');
+$headRef = getenv('GITHUB_HEAD_REF');
+
+if ($headRef === false) {
+    echo "No HEAD ref found";
+    exit(1);
+}//end if
+
+
+exec(sprintf('cd %; git diff --name-only %s..%s', getenv('PWD'), $headRef, $baseRef), $output, $return);
+var_dump($output);
+
