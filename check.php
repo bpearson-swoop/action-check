bf266a1e907707ff69443bece38fdcfed3e0ffce
Benjamin Pearson <ben.pearson@swoopanalytics.com>
2023-10-10 19:57:18 +1100

    git fetch

--- check.php
+++ check.php
@@ -15,31 +15,50 @@ if (!in_array($event, ['push', 'pull_request'])) {
     exit(1);
 }//end if
 
+
+$output  = [];
+$command = sprintf('git fetch -u origin %s', $baseRef);
+exec($command, $output, $return);
+var_dump($command);
+var_dump($output);
+
+$output  = [];
+$command = sprintf('git fetch -u origin %s', $headRef);
+exec($command, $output, $return);
+var_dump($command);
+var_dump($output);
+
+$output  = [];
 $command = sprintf('git diff --name-only origin/%s origin/%s', $baseRef, $headRef);
 exec($command, $output, $return);
 var_dump($command);
 var_dump($output);
 
+$output  = [];
 $command = 'git remote -v';
 exec($command, $output, $return);
 var_dump($command);
 var_dump($output);
 
+$output  = [];
 $command = sprintf('git rev-parse --abbrev-ref %s', $baseRef);
 exec($command, $output, $return);
 var_dump($command);
 var_dump($output);
 
+$output  = [];
 $command = sprintf('git rev-parse --abbrev-ref %s', $headRef);
 exec($command, $output, $return);
 var_dump($command);
 var_dump($output);
 
+$output  = [];
 $command = 'git rev-parse --abbrev-ref HEAD';
 exec($command, $output, $return);
 var_dump($command);
 var_dump($output);
 
+$output  = [];
 $command = 'git status';
 exec($command, $output, $return);
 var_dump($command);
